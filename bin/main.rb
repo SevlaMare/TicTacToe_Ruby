418cebd611300c2fdcd1a50f58b180929d41369e
SevlaMare <sevilamare@gmail.com>
2020-04-08 17:26:10 -0300

    meta game flow

--- bin/main.rb
+++ bin/main.rb
@@ -1,23 +1,23 @@
 #!/usr/bin/env ruby
 
-p("Welcome! Let's Play Tic Tac Toe")
+puts("Welcome! Let's Play Tic Tac Toe")
 
 # TODO: display board
-p('Board comming soon...')
+puts('Board comming soon...')
 
-p('Player 1 - Type your alias: ')
+puts('Player 1 - Type your alias: ')
 player1 = STDIN.gets.chomp
 
-p('Player 2 - Type your alias: ')
+puts('Player 2 - Type your alias: ')
 player2 = STDIN.gets.chomp
 
-p('Ready to start!')
+puts('Ready to start!')
 
-p("#{player1} make your move (1-9)")
+puts("#{player1} make your move (1-9)")
 # TODO: check input
 # TODO: if valid, transfer choice to board
 
-p("#{player2} make your move (1-9)")
+puts("#{player2} make your move (1-9)")
 # TODO: check input
 # TODO: if valid, transfer choice to board
 # TODO: match round to see if have a win or draw
