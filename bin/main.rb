f3a74987a1b90800657e69e7e810b67030e3c5fc
SevlaMare <sevilamare@gmail.com>
2020-04-08 10:13:11 -0300

    initial setup and todo's

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,26 @@
 #!/usr/bin/env ruby
-puts 'Hello world!'
-# get.chomp
+p("Welcome! Let's Play Tic Tac Toe")
+
+# TODO: display board
+p("Board comming soon...")
+
+p("Player 1 - Type your alias: ")
+player1 = STDIN.gets.chomp
+
+p("Player 2 - Type your alias: ")
+player2 = STDIN.gets.chomp
+
+p("Ready to start!")
+
+p("#{player1} make your move (1-9)")
+# TODO: check input
+# TODO: if valid, transfer choice to board
+
+
+p("#{player2} make your move (1-9)")
+# TODO: check input
+# TODO: if valid, transfer choice to board
+
+# TODO: match round to see if have a win or draw
+
+# TODO: if win show victory, else next round
