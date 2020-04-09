da9704c824707b5c7e783b8d198eb6a83b12e5a1
SevlaMare <sevilamare@gmail.com>
2020-04-08 21:17:24 -0300

    linter check

--- bin/main.rb
+++ bin/main.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/env ruby
 
-puts("Welcome! Let's Play Tic Tac Toe")
+puts("Welcome! Let's Play Tic Tac Toe\n\n")
 
 # TODO: display board
-puts('Board comming soon...')
+puts("Board comming soon...\n\n")
 
-puts('Player 1 - Type your alias: ')
+puts('Player 1 - Type your alias:')
 player1 = STDIN.gets.chomp
 
-puts('Player 2 - Type your alias: ')
+puts('Player 2 - Type your alias:')
 player2 = STDIN.gets.chomp
 
-puts('Ready to start!')
+puts("\n\nReady to start!")
 
 loop do
   puts("#{player1} make your move (1-9)")
@@ -22,9 +22,8 @@ loop do
   # TODO: check input
   # TODO: if valid, transfer choice to board
 
-  if state == 'end'
-    # TODO: check if has a win or if board become full
-    # otherwise play another match
-    break
-  end
+  puts("\n\n")
+  # TODO: check if has a win or if board become full
+  # otherwise play another match
+  break if true
 end
