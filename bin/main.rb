60c43265dda256c8adb6479cc189065d8f4cc8eb
SevlaMare <sevilamare@gmail.com>
2020-04-09 23:03:05 -0300

    board first draft

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,22 @@
 #!/usr/bin/env ruby
 
-puts("Welcome! Let's Play Tic Tac Toe\n\n")
+puts("#{' ' * 6}Welcome!\nLet's Play Tic Tac Toe")
+
+class Board
+  attr_accessor :field
+
+  def initialize
+    @field = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def display_board(field = @field)
+    puts(
+      "\n #{field[0]} | #{field[1]} | #{field[2]}"\
+      "\n #{field[3]} | #{field[4]} | #{field[5]}"\
+      "\n #{field[6]} | #{field[7]} | #{field[8]}"
+    )
+  end
+end
+
+game1 = Board.new
+game1.display_board
