33e20f6621364e4b7960a9213f9b030d886a9798
SevlaMare <sevilamare@gmail.com>
2020-04-11 17:04:15 -0300

    improve main loop

--- bin/main.rb
+++ bin/main.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 
-# TODO: implement private attributes wherever is possible
 class Board
   def initialize
     @field = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -16,55 +15,55 @@ class Board
 
   def board_update(choice, player)
     choice -= 1
-    @field[choice] = player == 1 ? 'x' : 'o'
+    @field[choice] = (player == 1 ? 'x' : 'o')
   end
 end
 
 class Engine < Board
-  def input_number(choice)
-    true if choice.between?(1, 9)
+  def input_number?(choice)
+    choice.between?(1, 9)
   end
 
   # return true if spot is already taken
-  def input_spot(choice, player)
-    spot = player == 2 ? 'x' : 'o'
-    puts 'Draw move! Spot already taken' if @field[choice - 1] == spot
+  def spot_taken?(choice, player)
+    @field[choice - 1] == 'x' || @field[choice - 1] == 'o'
   end
 
-  def win_or_draw
+  def win?
+    # TODO: check if is a winning move
     "\nPlayer #{player} Wins\nCongratulations!"
   end
-end
 
-# MAIN GAME LOOP
-play = true
-while play
-  puts("\n#{' ' * 6}Welcome!\nLet's Play Tic Tac Toe")
-
-  # BUILD GAME MATCH
-  game = Engine.new
-  game.board_display
-
-  # PLAY ROUND
-  (1..2).each do |i|
-    player = i
-    puts("\nPlayer #{i} make your move\n(Choice between 1-9):")
-
-    ask_input = true
-    while ask_input
-      choice = STDIN.gets.chomp.to_i
-
-      if game.input_number(choice)
-        game.input_spot(choice, player)
-
-        game.board_update(choice, player)
-        game.board_display
-
-        play = false
-        ask_input = false
-      else
-        puts 'Value Invalid, Try again (1-9):'
-      end
-    end
+  def draw
+    # TODO: trigger after 9 moves no winner
   end
 end
+
+game = Engine.new
+game.board_display
+
+round = 0
+while round <= 2
+  (1..2).each do |i|
+    player = i
+
+    loop do
+      puts("\nPlayer #{i} make your move\n(Choice between 1-9):")
+      choice = STDIN.gets.chomp.to_i
+
+      if game.input_number?(choice)
+        unless game.spot_taken?(choice, player)
+          game.board_update(choice, player)
+          game.board_display
+          break
+        else
+          puts "\nSpot already taken, try another"
+        end
+      end
+
+    end
+  end
+  puts "end round #{round}"
+  round += 1
+end
+puts "player 1 win"
