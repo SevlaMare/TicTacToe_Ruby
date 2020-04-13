c0af19bbc46140557e7bdaf2a661460ffa673caf
SevlaMare <sevilamare@gmail.com>
2020-04-13 16:41:44 -0300

    split win? method

--- lib/engine.rb
+++ lib/engine.rb
@@ -8,14 +8,8 @@ class Engine < Board
   end
 
   def win?(player)
-    win_arr = [
-      [0, 1, 2], [3, 4, 5], [6, 7, 8],
-      [0, 3, 6], [1, 4, 7], [2, 5, 8],
-      [0, 4, 8], [2, 4, 6]
-    ]
-
     mark = (player == 1 ? 'x' : 'o')
-    win_arr.each do |sub_array|
+    @win_arr.each do |sub_array|
       return true if sub_array.all? { |element| @field[element] == mark }
     end
     false
