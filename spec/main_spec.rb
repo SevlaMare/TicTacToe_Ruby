0fb6ec662f36b93263d30fea68406a34a34336d4
SevlaMare <sevilamare@gmail.com>
2020-04-14 00:27:50 -0300

    added tests

--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -6,10 +6,31 @@ require_relative '../lib/engine.rb'
 # describe engine do
 RSpec.describe Engine do # vscode
   let(:game1) { Engine.new }
-  let(:field1) { [1, 2, '3', 4, 5, 6, 7, 8, 9] }
+  let(:field_win) { ['x', 'x', 'x', 4, 5, 6, 7, 8, 9] }
 
-  describe '#input_number?' do
-    context 'Should not allow input out of range' do
+  describe '#Class Board' do
+    context 'Display Board' do
+      it 'Should display the board' do
+        expect(game1.board_display)
+          .to eq("\n 1 | 2 | 3\n 4 | 5 | 6\n 7 | 8 | 9")
+      end
+    end
+
+    context 'Update Board' do
+      it 'Player 1 can update the board' do
+        expect(game1.board_update(1, 1))
+          .to eq('x')
+      end
+
+      it 'Player 2 can update the board' do
+        expect(game1.board_update(1, 2))
+          .to eq('o')
+      end
+    end
+  end
+
+  describe 'Class Engine' do
+    context '#input_number?' do
       it 'First valid input' do
         expect(game1.input_number?(1))
           .to eq(true)
@@ -31,9 +52,24 @@ RSpec.describe Engine do # vscode
       end
     end
 
-    context 'Should not allow input non numeric' do
-      it 'Last valid input' do
-        expect(game1.spot_taken?(field1)).to eq(false)
+    context '#spot_taken?' do
+      it 'First valid input' do
+        expect(game1.spot_taken?(1))
+          .to eq(false)
+      end
+    end
+
+    context '#win?' do
+      it 'First valid input' do
+        expect(game1.win?(1))
+          .to eq(false)
+      end
+    end
+
+    context '#full_board?' do
+      it 'The board should not be full' do
+        expect(game1.full_board?)
+          .to eq(false)
       end
     end
   end
